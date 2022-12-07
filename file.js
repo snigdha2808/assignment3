4cd7f6a9e988ea7692a4a4f7a11ed9ac7f47111d
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-12-07 23:10:30 +0530

    Create file.js

---
+++ file.js
@@ -0,0 +1,9 @@
+const adderBtn  =   document.getElementById('adder-btn')
+
+
+document.getElementById ('new-ticket-section').style.display   =  "none";
+adderBtn.addEventListener('click',() =>
+{
+    document.getElementById('new-ticket-section').style.display  =   "";
+
+})
